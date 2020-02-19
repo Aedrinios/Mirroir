513527549f09bac252b7a7aa798050cfbaf91aaa
Aedrinios <clementclerc2@gmail.com>
2020-02-19 19:35:17 +0100

    working on player shoot

---
+++ Mirror/Assets/BulletBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviour : MonoBehaviour
+{
+    [SerializeField] private float bulletSpeed;
+    [SerializeField] private float bulletDamage;
+    [SerializeField] private float bulletLifeTime;
+
+    // Update is called once per frame
+    void Update()
+    {
+        bulletLifeTime -= Time.time;
+
+        if(bulletLifeTime <= 0)
+        {
+            DestroyBullet();
+        }
+
+        transform.position +=  transform.forward * Time.deltaTime * bulletSpeed;
+    }
+
+    void DestroyBullet()
+    {
+        Destroy(this);
+    }
+}
---
+++ Mirror/Assets/PlayerShoot.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class PlayerShoot : MonoBehaviour
+{
+    [SerializeField] private GameObject bulletPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(bulletPrefab, transform.position, transform.rotation);
+    }
+}
