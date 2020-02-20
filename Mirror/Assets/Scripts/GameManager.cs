0903bca2e2c54ba207cc4cc8f792801878bd3482
Kyohei <kalops22.kikoo@gmail.com>
2020-02-20 17:23:22 +0100

    Add GameManage and SpawnManager

---
+++ Mirror/Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Managers")]
+    public SpawnManager spawnManager;
+
+    [Space]
+    public int level = 1;
+
+    private void Start()
+    {
+        spawnManager.NewWave(level);
+    }
+
+    [ContextMenu("LaunchNextLevel")]
+    public void LaunchNextLevel()
+    {
+        level++;
+        spawnManager.NewWave(level);
+    }
+}
---
+++ Mirror/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Utilities;
+
+public class SpawnManager : MonoBehaviour
+{
+    public List<Transform> spawnsList;
+    private int ennemiSpawned;
+
+    [Space]
+    public GameObject prefabEnnemi;
+
+    public void NewWave(int level)
+    {
+        ennemiSpawned = 0;
+        Transform ennemi;
+        int indexSpawn;
+
+        for (int i = 0; i < level; i++)
+        {
+            if(ennemiSpawned >= spawnsList.Count)
+            {
+                ennemiSpawned = 0;
+            }
+
+            ennemi = Instantiate(prefabEnnemi).transform;
+            indexSpawn = Aleatoire.AleatoireBetween(0, spawnsList.Count - ennemiSpawned - 1);
+            ennemi.position = spawnsList[indexSpawn].position;
+            PlaceToEnd(spawnsList, indexSpawn);
+            ennemiSpawned++;
+        }
+    }
+
+    public void PlaceToEnd(List<Transform> list, int index)
+    {
+        Transform temp = list[index];
+        list.RemoveAt(index);
+        list.Add(temp);
+    }
+}
