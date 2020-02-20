2ad5aaea61153cf4a72034bad25e66c17c2518a2
Aedrinios <clementclerc2@gmail.com>
2020-02-20 12:58:11 +0100

    shoot and hit

---
+++ Mirror/Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthSystem : MonoBehaviour
+{
+    [SerializeField] private float health;
+
+    private void Update()
+    {
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+    private void Die()
+    {
+        Destroy(this.gameObject);
+    }
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+    }
+    
+}
